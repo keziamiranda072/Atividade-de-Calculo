943bdfbcb6fbe2f28747d8651f4b17091a4b00f1
keziamiranda072 <keziadeleno@gmail.com>
2025-07-17 14:07:42 -0300

    Update Exemplos de Codigos S.R

--- Exemplos de Codigos S.R
+++ Exemplos de Codigos S.R
@@ -1,1 +1,18 @@
+Exemplo 1:
+
+f = lambda x: x**2
+resultado = soma_riemann(f, 0, 1, 1000, tipo='meio')
+print(resultado)
+
+Exemplo 2:
+
+f1 = lambda x: x**0.5
+resultado1 = soma_riemann(f1, 0, 4, 1000, tipo='meio')
+print(resultado1)
+
+Exemplo 3:
+
+f2 = lambda x: math.exp(-x)
+resultado2 = soma_riemann(f2, 0, 1, 1000, tipo='meio')
+print(resultado2)
 
